8d8623e2368e24ae59d89c367d6b71151d2f5a9e
Rakesh Kumar <rakesh.sit06@gmail.com>
2025-08-31 22:56:57 +0530

    Delete api directory

--- api/authService.js
+++
@@ -1,4 +0,0 @@
-import axios from '../utils/axiosInstance';
-
-export const login = (credentials) => axios.post('/auth/login', credentials);
-export const register = (credentials) => axios.post('/auth/register', credentials);
--- api/profileService.js
+++
@@ -1,5 +0,0 @@
-import axios from '../utils/axiosInstance';
-
-export const getAccountDetails = () => axios.get('/account');
-export const updateProfile = (profileData) => axios.put('/account/update-account', profileData);   
-
